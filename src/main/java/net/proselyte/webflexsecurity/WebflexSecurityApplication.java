75b51aaa4530c23446d5da2435d1bf3923f7ea42
yrkash <yrkash@gmail.com>
2023-09-01 21:12:21 +0300

    Init commit. Gradle settings + dependencies

---
+++ src/main/java/net/proselyte/webflexsecurity/WebflexSecurityApplication.java
@@ -0,0 +1,12 @@
+package net.proselyte.webflexsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebflexSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebflexSecurityApplication.class, args);
+	}
+}
---
+++ src/test/java/net/proselyte/webflexsecurity/WebflexSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package net.proselyte.webflexsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebflexSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

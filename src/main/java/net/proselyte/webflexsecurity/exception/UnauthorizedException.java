7f4c60071ff1b056b5bd27be04f06fa406ef5187
yrkash <yrkash@gmail.com>
2023-09-16 23:41:07 +0300

    Token validation

---
+++ src/main/java/net/proselyte/webflexsecurity/exception/UnauthorizedException.java
@@ -0,0 +1,12 @@
+package net.proselyte.webflexsecurity.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+public class UnauthorizedException extends ApiException {
+    public UnauthorizedException(String message) {
+        super(message, "PROSELYTE_UNAUTHORIZED");
+    }
+}
